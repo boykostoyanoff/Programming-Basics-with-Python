e2b420e3a62e0b3f289b628844c6b56b5b86ab5a
boykostoyanoff <boykostoyanoff@gmail.com>
2023-07-27 09:22:37 +0300

    add task integer_operations.py

---
+++ data_types_and_variables/integer_operations.py
@@ -0,0 +1,10 @@
+number_1 = int(input())
+number_2 = int(input())
+number_3 = int(input())
+number_4 = int(input())
+
+number_1 += number_2
+number_1 = number_1 // number_3
+number_1 *= number_4
+
+print(number_1)
