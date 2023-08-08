b2bd607821d68915df6589577333f822579bf62f
boykostoyanoff <boykostoyanoff@gmail.com>
2023-08-08 19:29:48 +0300

    add task sort.py

---
+++ functions/min_max_sum.py
@@ -0,0 +1,5 @@
+numbers = list(map(lambda x: int(x), input().split(' ')))
+
+print(f'The minimum number is {min(numbers)}')
+print(f'The maximum number is {max(numbers)}')
+print(f'The sum number is: {sum(numbers)}')
--- functions/sort.py
+++ functions/sort.py
@@ -1,3 +1,2 @@
 numbers = list(map(lambda x: int(x), input().split(' ')))
-numbers = sorted(numbers, reverse=False)
-print(numbers)
+print(sorted(numbers))
