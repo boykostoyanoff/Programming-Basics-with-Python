d77eeeff79394808d27fd81803c7340de6769ba1
boykostoyanoff <boykostoyanoff@gmail.com>
2024-03-28 08:24:47 +0200

    add task judge.py

--- food/fruit.py
+++ food/fruit.py
@@ -1,4 +1,4 @@
-from project.food import Food
+from food.food import Food
 
 
 class Fruit(Food):
---
+++ single_inheritance/dog.py
@@ -0,0 +1,6 @@
+from single_inheritance.animal import Animal
+
+
+class Dog(Animal):
+    def bark(self):
+        return f"barking..."
