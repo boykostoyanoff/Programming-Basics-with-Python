c5d11aa21f356ef173875e48aca21190ac73b09a
boykostoyanoff <boykostoyanoff@gmail.com>
2024-04-01 17:22:31 +0300

    add task judge.py

---
+++ document_managment/storage.py
@@ -0,0 +1,78 @@
+from document_managment.category import Category
+from document_managment.document import Document
+from document_managment.topic import Topic
+
+
+class Storage:
+    def __init__(self):
+        self.categories = list()
+        self.topics = list()
+        self.documents = list()
+
+    def add_category(self, category: Category):
+        if category not in self.categories:
+            self.categories.append(category)
+
+    def add_topic(self, topic: Topic):
+        if topic not in self.topics:
+            self.topics.append(topic)
+
+    def add_document(self, document: Document):
+        if document not in self.documents:
+            self.documents.append(document)
+
+    def find_category_by_id(self, category_id):
+        for category in self.categories:
+            if category.id == category_id:
+                return category
+
+    def edit_category(self, category_id: int, new_name: str):
+        category = self.find_category_by_id(category_id)
+        if category:
+            category.name = new_name
+
+    def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str):
+        topic = self.find_topic_by_id(topic_id)
+        if topic:
+            topic.topic = new_topic
+            topic.storage_folder = new_storage_folder
+
+    def find_topic_by_id(self, topic_id):
+        for topic in self.topics:
+            if topic.id == topic_id:
+                return topic
+
+    def edit_document(self, document_id: int, new_file_name: str):
+        document = self.find_document_by_id(document_id)
+        if document:
+            document.file_name = new_file_name
+
+    def find_document_by_id(self, document_id):
+        for document in self.documents:
+            if document.id == document_id:
+                return document
+        return
+
+    def delete_category(self, category_id: int):
+        category = self.find_category_by_id(category_id)
+        if category:
+            self.categories.remove(category)
+
+    def delete_topic(self, topic_id):
+        topic = self.find_topic_by_id(topic_id)
+        if topic:
+            self.topics.remove(topic)
+
+    def delete_document(self, document_id: int):
+        document = self.find_document_by_id(document_id)
+        if document:
+            self.documents.remove(document)
+
+    def get_document(self, document_id: int):
+        return self.find_document_by_id(document_id)
+
+    def __repr__(self):
+        result = ''
+        for doc in self.documents:
+            result += str(doc) + '\n'
+        return result.strip('\n')
---
+++ movie_world/movie_world.py
@@ -0,0 +1,70 @@
+from movie_world.customer import Customer
+from movie_world.dvd import DVD
+
+
+class MovieWorld:
+    def __init__(self, name: str):
+        self.name = name
+        self.customers = list()
+        self.dvds = list()
+
+    @staticmethod
+    def dvd_capacity():
+        return 15
+
+    @staticmethod
+    def customer_capacity():
+        return 10
+
+    def add_customer(self, customer: Customer):
+        if len(self.customers) < self.customer_capacity():
+            self.customers.append(customer)
+
+    def add_dvd(self, dvd: DVD):
+        if len(self.dvds) < self.dvd_capacity():
+            self.dvds.append(dvd)
+
+    def rent_dvd(self, customer_id: int, dvd_id: int):
+        customer = self.find_customer_by_id(customer_id)
+        dvd = self.find_dvd_by_id(dvd_id)
+        if not customer or not dvd:
+            return
+        if dvd in customer.rented_dvds:
+            return f"{customer.name} has already rented {dvd.name}"
+        if dvd.is_rented:
+            return f"DVD is already rented"
+        if customer.age < dvd.age_restriction:
+            return f"{customer.name} should be at least {dvd.age_restriction} to rent this movie"
+        customer.rented_dvds.append(dvd)
+        dvd.is_rented = True
+        return f"{customer.name} has successfully rented {dvd.name}"
+
+    def return_dvd(self, customer_id: int, dvd_id: int):
+        customer = self.find_customer_by_id(customer_id)
+        dvd = self.find_dvd_by_id(dvd_id)
+        if not customer or not dvd:
+            return
+        if dvd in customer.rented_dvds:
+            dvd.is_rented = False
+            customer.rented_dvds.remove(dvd)
+            return f"{customer.name} has successfully returned {dvd.name}"
+        else:
+            return f"{customer.name} does not have that DVD"
+
+    def __repr__(self):
+        result = ''
+        for customer in self.customers:
+            result += customer.__repr__() + '\n'
+        for dvd in self.dvds:
+            result += dvd.__repr__() + '\n'
+        return result
+
+    def find_customer_by_id(self, customer_id):
+        for customer in self.customers:
+            if customer.id == customer_id:
+                return customer
+
+    def find_dvd_by_id(self, dvd_id):
+        for dvd in self.dvds:
+            if dvd.id == dvd_id:
+                return dvd
