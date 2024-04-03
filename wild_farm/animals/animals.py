37e62065ecd0ea2258af7efd3becdddfb8a40424
boykostoyanoff <boykostoyanoff@gmail.com>
2024-04-03 21:20:09 +0300

    add task judge.py

---
+++ wild_farm/animals/animals.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self, name: str, weight: float):
+        self.name = name
+        self.weight = weight
+        self.food_eaten = 0
+
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+    @abstractmethod
+    def feed(self, food):
+        pass
+
+
+class Bird(Animal, ABC):
+    def __init__(self, name, weight, wing_size: float):
+        super().__init__(name, weight)
+        self.wing_size = wing_size
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} [{self.name}, {self.wing_size}, {self.weight}, {self.food_eaten}]"
+
+
+class Mammal(Animal, ABC):
+    FOOD_EAT = 0
+    WEIGHT_INCREASE_PER_PIECE = 0
+
+    def __init__(self, name, weight, living_region: str):
+        super().__init__(name, weight)
+        self.living_region = living_region
+
+    def feed(self, food):
+        if type(food) in self.FOOD_EAT:
+            self.weight += food.quantity * self.WEIGHT_INCREASE_PER_PIECE
+            self.food_eaten += food.quantity
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} [{self.name}, {self.weight}, {self.living_region}, {self.food_eaten}]"
